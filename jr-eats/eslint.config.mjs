ed95782b4a693cdeaa5b0f5da40e6b7fcc7b9a8a
Jerry Chen <jjerry.chen@mail.utoronto.ca>
2025-05-28 16:03:19 -0400

    more eslint

--- jr-eats/eslint.config.mjs
+++ jr-eats/eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,14 +12,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended, // or `strict` or any custom set you're using
-  {
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
     rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
+      'no-unused-vars': 'off',
     },
-  },
-];
-
+  }),
+]
+ 
+export default eslintConfig
